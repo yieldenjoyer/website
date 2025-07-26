bf5ce8cb97046ac9f656aebfbf7db1c89d85d00c
0xpepe <agoodwin848@gmail.com>
2025-07-25 20:24:49 -0400

    Delete interfaces directory

--- interfaces/IPendleStandardizedYield.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
-
-import "./IERC20.sol";
-
-/**
- * @title IPendleStandardizedYield
- * @dev Interface for Pendle Standardized Yield (SY) tokens
- * @notice SY tokens are wrappe
